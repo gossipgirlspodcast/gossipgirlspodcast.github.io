0229b82c83c55ce2fa6fe9e3fefa746b6b53ccc8
Subi Shah <nsubishah@gmail.com>
2017-01-10 20:49:12 -0600

    form update

--- js/main.js
+++ js/main.js
@@ -57,12 +57,30 @@ function backButton () {
 
 
 // AJAX FOR EPISODES
+//RSS FEED 
+//http://simplecast.com/podcasts/1671/rss
+
 
 $.ajax({
-  url: "https://gossipgirls.simplecast.fm/",
-  context: document.body
+  url: "https://www.gossipgirls.simplecast.fm/",
+  context: document.body,
+    xhrFields: {
+    // The 'xhrFields' property sets additional fields on the XMLHttpRequest.
+    // This can be used to set the 'withCredentials' property.
+    // Set the value to 'true' if you'd like to pass cookies to the server.
+    // If this is enabled, your server must respond with the header
+    // 'Access-Control-Allow-Credentials: true'.
+    withCredentials: false
+  },
+
 }).done(function(data) {
 console.log('data')});
 
+// var xdr = new XDomainRequest();
+// xdr.open("get", "https://www.gossipgirls.simplecast.fm/");
+// xdr.onload = function(data){
+// 	console.log(data);
+//     //do something
+// };
 
 

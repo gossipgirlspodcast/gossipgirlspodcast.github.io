b697f4899b81aaa6b7fbc363a15078ae53f8c329
Subi Shah <nsubishah@gmail.com>
2017-01-19 19:25:42 -0600

    more css udpates

--- js/main.js
+++ js/main.js
@@ -51,37 +51,37 @@ function backButton () {
 
 // TOP NAVIGATION
 
-$(function() {
+// $(function() {
     // Stick the #nav to the top of the window
     var nav = $('#nav');
     var navHomeY = nav.offset().top;
     var isFixed = false;
-    var $w = $(window);
-    $w.scroll(function() {
-        var scrollTop = $w.scrollTop();
+    var w = $(window);
+    w.scroll(function() {
+        var scrollTop = w.scrollTop();
         var shouldBeFixed = scrollTop > navHomeY;
         if (shouldBeFixed && !isFixed) {
             nav.css({
                 position: 'fixed',
-                top: 10,
+                top: 0,
+                // 'paddingTop', '10px'
                 left: nav.offset().left,
                 width: nav.width(),
+                background: 'black',
             });
-
-
             isFixed = true;
         }
         else if (!shouldBeFixed && isFixed)
         {
             nav.css({
                 position: 'static'
+
             });
 
             isFixed = false;
         }
     });
-});
+// });
 
-//ADD JS TO CREATE A TINY LOGO OF GOSSIP GIRLS WHEN THE TOP LOGO IS MISSING
 
 
